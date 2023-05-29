13e43525cb98a660cc7f2452506868d189587baa
Chutipong Roobklom <khunemz@gmail.com>
2023-05-29 23:13:13 +0700

    add customer collection

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
+use App\Http\Resources\V1\CustomerCollection;
 use App\Http\Resources\V1\CustomerResource;
 use App\Models\Customer;
 
@@ -18,7 +19,7 @@ class CustomerController extends Controller
     public function index()
     {
         //
-        return Customer::all();
+        return new CustomerCollection(Customer::all());
     }
 
     /**
