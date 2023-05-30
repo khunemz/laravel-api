f24746a61481f41fce5e25fbec2ad6262d1b2822
Chutipong Roobklom <khunemz@gmail.com>
2023-05-30 13:08:26 +0700

    [fea]: utilize resources

--- app/Http/Controllers/Api/V1/CustomerController.php
+++ app/Http/Controllers/Api/V1/CustomerController.php
@@ -22,15 +22,13 @@ class CustomerController extends Controller
     {
         //
         $filter = new CustomersFilter();
-        $queryItems = $filter->transform($request); //[['column', 'operator', 'value']]
-
-        if (count($queryItems) == 0) {
-            return new CustomerCollection(Customer::paginate());
-        } else {
-            $customers = Customer::where($queryItems)->paginate();
-
-            return new CustomerCollection($customers->appends($request->query()));
+        $filterItems = $filter->transform($request);
+        $includeInvoices = $request->query('includeInvoices');
+        $customers = Customer::where($filterItems);
+        if ($includeInvoices) {
+            $customers = $customers->with('invoices');
         }
+        return new CustomerCollection($customers->paginate()->appends($request->query()));
     }
 
     /**
@@ -63,6 +61,10 @@ class CustomerController extends Controller
     public function show(Customer $customer)
     {
         //
+        $includeInvoices = request()->query('includeInvoices');
+        if ($includeInvoices) {
+            return new CustomerResource($customer->loadMissing('invoices'));
+        }
         return new CustomerResource($customer);
     }
 
--- app/Http/Resources/V1/CustomerResource.php
+++ app/Http/Resources/V1/CustomerResource.php
@@ -14,7 +14,6 @@ class CustomerResource extends JsonResource
      */
     public function toArray($request)
     {
-        // return parent::toArray($request);
         return [
             'id' => $this->id,
             'name' => $this->name,
@@ -24,6 +23,7 @@ class CustomerResource extends JsonResource
             'city' => $this->city,
             'state' => $this->state,
             'postalCode' => $this->postal_code,
+            'invoices' => InvoiceResource::collection($this->whenLoaded('invoices')),
         ];
     }
 }
